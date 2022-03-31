cfd744efd950914a82e4a9a9e56142578cefb4d5
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-03-31 21:51:53 +0200

    Finish lesson

--- src/Picshare.elm
+++ src/Picshare.elm
@@ -1,7 +1,31 @@
 module Picshare exposing (main)
 
-import Html exposing (Html, div, text)
+import Html exposing (..)
+import Html.Attributes exposing (class, src)
+
 
 main : Html msg
-main = 
-    div [] [text "Picshare"]
+main =
+    div []
+        [ div [ class "header" ]
+            [ h1 [] [ text "Picshare" ] ]
+        , div [ class "content-flow" ]
+            [ viewDeailtedPhoto (baseUrl ++ "1.jpg") "Surfing"
+            , viewDeailtedPhoto (baseUrl ++ "2.jpg") "The Fox"
+            , viewDeailtedPhoto (baseUrl ++ "3.jpg") "Evening"
+            ]
+        ]
+
+
+baseUrl : String
+baseUrl =
+    "https://programming-elm.com/"
+
+
+viewDeailtedPhoto : String -> String -> Html msg
+viewDeailtedPhoto url caption =
+    div [ class "detailed-photo" ]
+        [ img [ src url ] []
+        , div [ class "photo-info" ]
+            [ h2 [ class "caption" ] [ text caption ] ]
+        ]

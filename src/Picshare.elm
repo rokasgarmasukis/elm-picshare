bba44687e37ae6edc453e927912cd5ecef808341
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-03-31 22:38:32 +0200

    Refactor, make Program

--- src/Picshare.elm
+++ src/Picshare.elm
@@ -1,20 +1,18 @@
 module Picshare exposing (main)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
 
 
-main : Html msg
+main : Program () { url : String, caption : String, liked : Bool } Msg
 main =
-    div []
-        [ div [ class "header" ]
-            [ h1 [] [ text "Picshare" ] ]
-        , div [ class "content-flow" ]
-            [ viewDeailtedPhoto (baseUrl ++ "1.jpg") "Surfing"
-            , viewDeailtedPhoto (baseUrl ++ "2.jpg") "The Fox"
-            , viewDeailtedPhoto (baseUrl ++ "3.jpg") "Evening"
-            ]
-        ]
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
 
 
 baseUrl : String
@@ -22,10 +20,71 @@ baseUrl =
     "https://programming-elm.com/"
 
 
-viewDeailtedPhoto : String -> String -> Html msg
-viewDeailtedPhoto url caption =
+initialModel : { url : String, caption : String, liked : Bool }
+initialModel =
+    { url = baseUrl ++ "1.jpg"
+    , caption = "Surfing"
+    , liked = False
+    }
+
+
+type Msg
+    = Like
+    | Unlike
+
+
+update :
+    Msg
+    -> { url : String, caption : String, liked : Bool }
+    -> { url : String, caption : String, liked : Bool }
+update msg model =
+    case msg of
+        Like ->
+            { model | liked = True }
+
+        Unlike ->
+            { model | liked = False }
+
+
+view : { url : String, caption : String, liked : Bool } -> Html Msg
+view model =
+    div []
+        [ div [ class "header" ]
+            [ h1 [] [ text "Picshare" ] ]
+        , div [ class "content-flow" ]
+            [ viewDetailedPhoto model
+            ]
+        ]
+
+
+viewDetailedPhoto : { url : String, caption : String, liked : Bool } -> Html Msg
+viewDetailedPhoto model =
+    let
+        buttonClass =
+            if model.liked then
+                "fa-heart"
+
+            else
+                "fa-heart-o"
+
+        msg =
+            if model.liked then
+                Unlike
+
+            else
+                Like
+    in
     div [ class "detailed-photo" ]
-        [ img [ src url ] []
+        [ img [ src model.url ] []
         , div [ class "photo-info" ]
-            [ h2 [ class "caption" ] [ text caption ] ]
+            [ div [ class "like-button" ]
+                [ i
+                    [ class "fa fa-2x"
+                    , class buttonClass
+                    , onClick msg
+                    ]
+                    []
+                ]
+            , h2 [ class "caption" ] [ text model.caption ]
+            ]
         ]
